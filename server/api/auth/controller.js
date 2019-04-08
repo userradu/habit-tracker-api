f61f7c8b3826052cf45f544a89710d5eae3e101a
radu <userradu91@gmail.com>
2019-04-08 21:24:02 +0300

    Refactor code: signup

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -5,85 +5,128 @@ const nodemailer = require('nodemailer');
 const User = require('../user/userModel');
 const pug = require('pug');
 const Joi = require('joi');
-const { signupSchema } = require('./validationSchemas');
+const { signupSchema, verifyAccountSchema } = require('./validationSchemas');
 const HttpError = require('../exceptions/httpError');
 const utils = require('../../utils/utils')
 
-const saltRounds = 10;
+function generatePasswordHash(password) {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+}
 
-exports.signup = function (req, res, next) {
-
-    Joi.validate(req.body, signupSchema, { abortEarly: false }, (err, value) => {
-        if (err) return next(new HttpError(400, utils.getJoiErrorMessages(err)));
-
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-            if (err) return next(err);
-
-            crypto.randomBytes(20, (err, buffer) => {
-                if (err) return next(err);
-
-                let verificationToken = buffer.toString('hex');
-
-                const user = new User({
-                    email: req.body.email,
-                    password: hash,
-                    verificationToken: verificationToken,
-                    verified: false
-                });
-
-                user.save((err, user) => {
-
-                    if (err) return next(err);
-
-                    var transporter = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: config.mail.username,
-                            pass: config.mail.password
-                        }
-                    });
-
-                    var mailOptions = {
-                        from: config.mail.username,
-                        to: req.body.email,
-                        subject: 'Confirm account',
-                        html: pug.renderFile('server/api/auth/views/verifyEmailTemplate.pug', {
-                            url: config.accountActivationEmail
-                        })
-                    };
-
-                    transporter.sendMail(mailOptions, function (err, info) {
-                        if (err) return next(err);
-
-                        console.log(`An email was sent to ${req.body.email}`);
-                        return res.status(201).json({
-                            status: 'success',
-                            message: 'Account created'
-                        });
-                    });
-                });
-            });
+function generateVerificationToken() {
+    return new Promise((resolve, reject) => {
+        crypto.randomBytes(20, (err, buffer) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(buffer.toString('hex'));
+            }
         });
     })
+}
+
+function createAccount(email, password, token) {
+    const user = new User({
+        email: email,
+        password: password,
+        verificationToken: token,
+        verified: false
+    });
+
+    return user.save();
+}
+
+function sendAccountConfirmationEmail(email) {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: config.mail.username,
+            pass: config.mail.password
+        }
+    });
+
+    var mailOptions = {
+        from: config.mail.username,
+        to: email,
+        subject: 'Confirm account',
+        html: pug.renderFile('server/api/auth/views/verifyEmailTemplate.pug', {
+            url: config.accountActivationEmail
+        })
+    };
+
+    return transporter.sendMail(mailOptions);
+}
+
+
+exports.signup = function (req, res, next) {
+    let passwordHash;
+
+    Joi.validate(req.body, signupSchema, { abortEarly: false })
+        .then(() => {
+            return generatePasswordHash(req.body.password);
+        })
+        .then((hash) => {
+            passwordHash = hash;
+            return generateVerificationToken();
+        })
+        .then((token) => {
+            return createAccount(req.body.email, passwordHash, token);
+        })
+        .then(() => {
+            return sendAccountConfirmationEmail(req.body.email);
+        })
+        .then(() => {
+            return res.status(201).json({
+                status: 'success',
+                message: 'Account created'
+            });
+        })
+        .catch((err) => {
+            if (err.isJoi) {
+                next(new HttpError(400, utils.getJoiErrorMessages(err)));
+            }
+            else {
+                next(err);
+            }
+        });
 };
 
+function getUser(query) {
+    return User.findOne(query);
+}
+
 exports.verifyAccount = function (req, res, next) {
-    let email = req.body.email;
-    let verificationToken = req.body.verificationToken;
-    User.findOne({ email: email, verificationToken: verificationToken }, (err, user) => {
-        if (err) return next(err);
+    Joi.validate(req.body, verifyAccountSchema, { abortEarly: false })
+        .then(() => {
+            let email = req.body.email;
+            let verificationToken = req.body.verificationToken;
 
-        if (!user) return next(new HttpError(404, "The account doesn't exists"));
-
-        user.verified = true;
-        user.verificationToken = null;
-        user.save((err, user) => {
-            if (err) return next(err);
-
+            return getUser({ email: email, verificationToken: verificationToken });
+        })
+        .then((user) => {
+            if (!user) {
+                throw new HttpError(404, "The account doesn't exists");
+            }
+            else {
+                user.verified = true;
+                user.verificationToken = null;
+                return user.save();
+            }
+        })
+        .then(() => {
             return res.json({
                 status: 'success',
                 message: 'Account verified'
             });
         })
-    });
+        .catch((err) => {
+            if (err.isJoi) {
+                next(new HttpError(400, utils.getJoiErrorMessages(err)));
+            }
+            else {
+                next(err);
+            }
+        });
 };
--- server/api/auth/validationSchemas.js
+++ server/api/auth/validationSchemas.js
@@ -30,3 +30,36 @@ exports.signupSchema = Joi.object().keys({
     })
 });
 
+exports.verifyAccountSchema = Joi.object().keys({
+    email: Joi.string().email().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.type) {
+                case "any.required":
+                    err.message = "The email is required";
+                    break;
+                case "string.email":
+                    err.message = "The email is not valid";
+                    break;
+                default:
+                    break;
+            }
+        });
+        return errors
+    }),
+    verificationToken: Joi.string().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.type) {
+                case "any.required":
+                    err.message = "The verification token is required";
+                    break;
+                case "string.base":
+                    err.message = "The verification token must be a string";
+                    break;
+                default:
+                    break;
+            }
+        });
+        return errors;
+    })
+});
+
