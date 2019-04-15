f93387124b6cae620982654a2e7c4b3c899653d7
radu <userradu91@gmail.com>
2019-04-15 22:05:38 +0300

    Add more tests for the /signup endpoint

--- test/auth.js
+++ test/auth.js
@@ -21,4 +21,39 @@ describe('Signup', () => {
                 done();
             })
     });
+
+    it('shoud throw an error if the required fields are not specified', (done) => {
+        request(app).post('/auth/signup')
+            .send({})
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(400);
+                done();
+            })
+    });
+
+    it('shoud throw an error if the email is not valid', (done) => {
+        request(app).post('/auth/signup')
+            .send({
+                email: "invalidEmail",
+                password: "password",
+                confirm_password: "password"
+            })
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(400);
+                done();
+            })
+    });
+
+    it('shoud throw an error if the passwords do not match', (done) => {
+        request(app).post('/auth/signup')
+            .send({
+                email: "userradu91@gmail.com",
+                password: "password",
+                confirm_password: "another password"
+            })
+            .end((err, res) => {
+                expect(res.statusCode).to.equal(400);
+                done();
+            })
+    });
 });
