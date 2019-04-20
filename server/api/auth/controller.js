3b19d405999d482ff0f35a02418cdc275d3eb6fb
radu <userradu91@gmail.com>
2019-04-20 10:06:49 +0300

    Use async await

--- server/api/auth/controller.js
+++ server/api/auth/controller.js
@@ -61,82 +61,66 @@ function sendAccountConfirmationEmail(email, token) {
 }
 
 
-exports.signup = function (req, res, next) {
-    let passwordHash;
-    let activationToken;
+exports.signup = async function (req, res, next) {
 
-    Joi.validate(req.body, signupSchema, { abortEarly: false })
-        .then(() => {
-            return getUser({ email: req.body.email });
-        })
-        .then((user) => {
-            if (user) {
-                throw new HttpError(409, "The email is taken");
-            }
-        })
-        .then(() => {
-            return generatePasswordHash(req.body.password);
-        })
-        .then((hash) => {
-            passwordHash = hash;
-            return generateVerificationToken();
-        })
-        .then((token) => {
-            activationToken = token;
-            return createAccount(req.body.email, passwordHash, token);
-        })
-        .then(() => {
-            return sendAccountConfirmationEmail(req.body.email, activationToken);
-        })
-        .then(() => {
-            return res.status(201).json({
-                status: 'success',
-                message: 'Account created'
-            });
-        })
-        .catch((err) => {
-            if (err.isJoi) {
-                next(new HttpError(400, utils.getJoiErrorMessages(err)));
-            }
-            else {
-                next(err);
-            }
+    try {
+
+        await Joi.validate(req.body, signupSchema, { abortEarly: false })
+        const user = await getUser({ email: req.body.email });
+
+        if (user) {
+            throw new HttpError(409, "The email is taken");
+        }
+
+        const passwordHash = await generatePasswordHash(req.body.password);
+        const activationToken = await generateVerificationToken();
+        await createAccount(req.body.email, passwordHash, activationToken);
+        await sendAccountConfirmationEmail(req.body.email, activationToken);
+
+        return res.status(201).json({
+            status: 'success',
+            message: 'Account created'
         });
+
+    } catch (error) {
+        if (error.isJoi) {
+            next(new HttpError(400, utils.getJoiErrorMessages(error)));
+        }
+        else {
+            next(error);
+        }
+    }
 };
 
 function getUser(query) {
     return User.findOne(query);
 }
 
-exports.verifyAccount = function (req, res, next) {
-    Joi.validate(req.body, verifyAccountSchema, { abortEarly: false })
-        .then(() => {
-            let verificationToken = req.body.verificationToken;
+exports.verifyAccount = async function (req, res, next) {
+    try {
+        await Joi.validate(req.body, verifyAccountSchema, { abortEarly: false });
 
-            return getUser({ verificationToken: verificationToken });
-        })
-        .then((user) => {
-            if (!user) {
-                throw new HttpError(404, "The account doesn't exists");
-            }
-            else {
-                user.verified = true;
-                user.verificationToken = null;
-                return user.save();
-            }
-        })
-        .then(() => {
-            return res.json({
-                status: 'success',
-                message: 'Account verified'
-            });
-        })
-        .catch((err) => {
-            if (err.isJoi) {
-                next(new HttpError(400, utils.getJoiErrorMessages(err)));
-            }
-            else {
-                next(err);
-            }
+        const verificationToken = req.body.verificationToken;
+        const user = await getUser({ verificationToken: verificationToken });
+
+        if (!user) {
+            throw new HttpError(404, "The account doesn't exists");
+        }
+
+        user.verified = true;
+        user.verificationToken = null;
+        await user.save();
+
+        return res.json({
+            status: 'success',
+            message: 'Account verified'
         });
+    } catch (error) {
+        if (error.isJoi) {
+            next(new HttpError(400, utils.getJoiErrorMessages(error)));
+        }
+        else {
+            next(error);
+        }
+    }
 };
