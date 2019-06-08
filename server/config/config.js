45b763bd462bc6858edffcf666aa1a68fbfd131d
radu <userradu91@gmail.com>
2019-06-08 18:21:28 +0300

    Change default port

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const config = {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
   accountActivationPageUrl: process.env.ACCOUNT_ACTIVATION_PAGE_URL,
   resetPasswordUrl: process.env.RESET_PASSWORD_PAGE_URL,
   jwt_secret: process.env.JWT_SECRET
